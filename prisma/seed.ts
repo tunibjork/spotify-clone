0e4d98a14eccf81118abafaa4feb62c84cf6b59c
Samuel Fanhais <20503679+tunibjork@users.noreply.github.com>
2022-10-03 16:42:34 +0200

    finish db seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,62 @@
-import { SkeletonText } from "@chakra-ui/react";
-import { PrismaClient, Song } from "@prisma/client";
-import brypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { url } from "inspector";
 import { artistsData } from "./songsData";
 
 const prisma = new PrismaClient();
 
 const run = async () => {
-  await Promise.all(artistsData.map(async (artist) => {
-    return prisma.artist.upsert({
-      where: {name: artist.name},
-      update: {},
-      create: {
-        name: artist.name,
-        songs: {
-          create: artist.songs.map(song => ({
-            name: song.name,
-            duration: song.duration,
-            url: song.url,
-          }))
-        }
-      }
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => {
+              return {
+                name: song.name,
+                duration: song.duration,
+                url: song.url,
+              };
+            }),
+          },
+        },
+      });
     })
-  })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "user@text.com" },
+    update: {},
+    create: {
+      email: "user@text.com",
+      password: bcrypt.hashSync("password", salt),
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => {
+              return {
+                id: song.id,
+              };
+            }),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
