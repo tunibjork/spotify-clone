35a38b81b21070fcfe208534ea244a2977e45bd6
Samuel Fanhais <20503679+tunibjork@users.noreply.github.com>
2022-10-04 15:02:27 +0200

    adding content to authForm component and handling form submission

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,26 +1,61 @@
-import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { Box, Flex, Input, Button, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useSWRConfig } from "swr";
+import NextImage from "next/image";
 import { auth } from "../lib/mutations";
 
-interface AuthFormProps {
-  mode: string;
-}
-
-export default function AuthForm({ mode }: { mode: AuthFormProps }) {
+export default function AuthForm({ mode }: { mode: "signup" | "signin" }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
+  async function handleSubmit(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    const user = await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push("/");
+  }
+
   return (
     <Box width="100vw" height="100vh" bg="black" color="white">
-      <Flex justify="center" align="center" height="100px">
-        Hello
-      </Flex>
-      <Flex justify="center" align="center" height="calc(100vh-100px)">
-        Form
+      {/* <Flex justify="center" align="center" height="100px">
+        Hello {mode}
+      </Flex> */}
+      <Flex justify="center" align="center" height="100vh">
+        <Box padding={12} bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4}>
+              <Box paddingBottom={8}>
+                <NextImage src="/spotify_logo.svg" height={60} width={150} />
+              </Box>
+              <Input
+                placeholder="email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                size="lg"
+              />
+              <Input
+                placeholder="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                size="lg"
+              />
+              <Button
+                type="submit"
+                size="lg"
+                bg="green.600"
+                isLoading={isLoading}
+                _hover={{ bg: "green.400" }}
+              >
+                {mode === "signin" && "Sign In"}
+                {mode === "signup" && "Sign Up"}
+              </Button>
+            </VStack>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
