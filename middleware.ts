63a8457463f90766694d86bd3c8d9c767e76658f
Samuel Fanhais <20503679+tunibjork@users.noreply.github.com>
2022-10-05 14:50:23 +0200

    add protected paths to edge middleware

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,27 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-  url.pathname = "/signin";
   const token = req.cookies.get("SPOTIFY_CLONE_ACCESS_TOKEN");
+  const url = req.nextUrl.clone();
 
-  if (!token) {
-    return NextResponse.redirect(url);
+  if (
+    req.nextUrl.pathname === "/" ||
+    req.nextUrl.pathname.startsWith("/playlist") ||
+    req.nextUrl.pathname.startsWith("/library")
+  ) {
+    if (!token) {
+      url.pathname = "/signin";
+      return NextResponse.redirect(url);
+    }
+  }
+
+  if (
+    req.nextUrl.pathname === "/signin" ||
+    req.nextUrl.pathname === "/signup"
+  ) {
+    if (token) {
+      url.pathname = "/";
+      return NextResponse.redirect(url);
+    }
   }
 }
-
-export const config = {
-  matcher: ["/", "/playlist", "/library"],
-};
