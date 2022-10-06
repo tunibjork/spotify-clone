5930ee0436c7724efc9d9258a6408f9a670acfff
Samuel Fanhais <20503679+tunibjork@users.noreply.github.com>
2022-10-06 10:46:55 +0200

    fix type signature for component props and render logic

--- components/SideMenuItem.tsx
+++ components/SideMenuItem.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import NextLink from "next/link";
 import { ListItem, ListIcon, LinkBox, LinkOverlay } from "@chakra-ui/layout";
 import { IconType } from "react-icons";
+import { MdOutlineQueueMusic } from "react-icons/md";
 
 interface SideMenuItemProps {
   name: string;
-  icon: IconType;
-  route: string;
-  children?: React.ReactNode;
+  icon?: IconType;
+  route?: string;
+  // children?: React.ReactNode;
 }
 
 export default function SideMenuItem({ item }: { item: SideMenuItemProps }) {
   return (
     <ListItem key={item.name} fontSize="16px" fontWeight="medium">
       <LinkBox>
-        <NextLink href={item.route} passHref>
+        <NextLink href={item.route || "/"} passHref>
           <LinkOverlay>
-            {item.icon && <ListIcon as={item.icon} color="white" />}
+            <ListIcon as={item.icon || MdOutlineQueueMusic} color="white" />
             {item.name}
           </LinkOverlay>
         </NextLink>
