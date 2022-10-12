71fec559b0b0a1e92cf34726a2a57ce8aacbe0b7
Samuel Fanhais <20503679+tunibjork@users.noreply.github.com>
2022-10-12 23:55:38 +0200

    add index.tsx layout features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,56 @@
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 import { Artist } from "@prisma/client";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import styles from "../styles/Home.module.css";
 import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
+import { useMe, usePlaylistCount } from "../lib/hooks";
 
 interface HomeProps {
   artists: Artist[];
 }
 
 export default function Home({ artists }: HomeProps) {
+  const { user } = useMe();
+  const { playlistCount } = usePlaylistCount();
   return (
     <GradientLayout
       color="yellow"
-      image=""
-      roundImage={false}
+      image="/tunibjork.jpg"
+      roundImage
       subtitle="profile"
-      title="Samuel Fanhais"
-      details="15 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      details={`${playlistCount} public playlists`}
     >
-      <div>children</div>
+      <Box padding="40px" color="white">
+        <Box>
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box width="200px" paddingRight="20px" marginTop="20px">
+              <Box bg="gray.900" borderRadius="6px" padding="20px" width="100%">
+                <Image
+                  src="/tunibjork.jpg"
+                  borderRadius="100%"
+                  boxShadow="xl"
+                />
+                <Box marginTop="10px">
+                  <Text fontSize="large" fontWeight="bold">
+                    {artist.name}
+                  </Text>
+                  <Text fontSize="xs">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
 }
